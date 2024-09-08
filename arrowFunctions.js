778fff07f766244b6243856fd617fecf00863951
Kirill Arkhipov <k.arkhipov@innopolis.university>
2024-09-08 21:17:03 +0300

    arrow functions and formater, but does not works

---
+++ arrowFunctions.js
@@ -0,0 +1,13 @@
+let func = (a, b, c) => {
+  alert(a > 0 ? a : b);
+};
+
+// func(1, 2, 3); // 1
+
+let sum = (a, b) => a + b;
+
+// alert(sum(1, 2));
+
+let double = (n) => n * 2;
+
+// alert(double(2));
